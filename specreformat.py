369aeaa9c14995fb39ad352e1e64eb1078e2c08c
zyyin <alfred>
2020-10-23 10:39:15 +0800

    spec import

--- specreformat.py
+++ specreformat.py
@@ -1,14 +1,13 @@
 import os
+from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
-from dataparepare import *
-from interfere import *
-from similarity import *
-# from oldsimi import *
 from pyspark.sql.types import *
-from pyspark.sql import functions as func
+import re
+from pyspark.sql.functions import *
 from pyspark.sql import Window
-from pyspark.sql.functions import pandas_udf, PandasUDFType
+from pyspark.sql.functions import col, pandas_udf, PandasUDFType
 from pyspark.sql.types import *
+import pandas as pd
 
 
 
