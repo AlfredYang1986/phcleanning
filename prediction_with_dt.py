a0130d317a1c99726f10a9e18c01db43d2435fdd
AlfredYang1986 <yangyuanpig@gmail.com>
2020-11-02 19:28:35 +0800

    验证算法

--- prediction_with_dt.py
+++ prediction_with_dt.py
@@ -22,7 +22,7 @@ from pyspark.ml.classification import MultilayerPerceptronClassificationModel
 from pyspark.ml.classification import DecisionTreeClassificationModel
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml import PipelineModel
-from pdu_feature import similarity
+from pdu_feature import similarity, hit_place_prediction
 
 
 def prepare():
@@ -78,7 +78,7 @@ if __name__ == '__main__':
 	result = result.withColumn("JACCARD_DISTANCE_MOLE_NAME", result.JACCARD_DISTANCE[0]) \
 				.withColumn("JACCARD_DISTANCE_DOSAGE", result.JACCARD_DISTANCE[1]) \
 				.drop("JACCARD_DISTANCE", "features", "indexedFeatures").drop("rawPrediction", "probability")
-	result.orderBy("id").repartition(1).write.mode("overwrite").csv("s3a://ph-max-auto/2020-08-11/BPBatchDAG/refactor/alfred/tmp/result")
+	# result.orderBy("id").repartition(1).write.mode("overwrite").csv("s3a://ph-max-auto/2020-08-11/BPBatchDAG/refactor/alfred/tmp/result")
 	df_ph = result.where((result.prediction == 1.0) | (result.label == 1.0))
 	ph_total = result.groupBy("id").agg({"prediction": "first", "label": "first"}).count()
 	print(ph_total)
@@ -89,14 +89,52 @@ if __name__ == '__main__':
 
 	ph_positive_prodict = df_true_positive.count()
 	print(ph_positive_prodict)
-	ph_positive_hit = result.where((df_true_positive.prediction == result.label) & (result.label == 1.0)).count()
+	ph_positive_hit = result.where((result.prediction == result.label) & (result.label == 1.0)).count()
 	print(ph_positive_hit)
 	# ph_negetive_hit = result.where(result.prediction != result.label).count()
 	print("Pharbers Test set accuracy = " + str(ph_positive_hit / ph_total))
 	print("Pharbers Test set precision = " + str(ph_positive_hit / ph_positive_prodict))
 
-	df_true_positive.orderBy("id").repartition(1).where((result.prediction == 0.0) & (result.label == 1.0)).write.mode("overwrite").option("header", "true").csv("s3a://ph-max-auto/2020-08-11/BPBatchDAG/refactor/alfred/tmp/false_negative")
-	df_true_positive.orderBy("id").repartition(1).where((result.prediction == 1.0) & (result.label == 0.0)).write.mode("overwrite").option("header", "true").csv("s3a://ph-max-auto/2020-08-11/BPBatchDAG/refactor/alfred/tmp/false_positive")
+	# for analysis
+	# df_true_positive.orderBy("id").repartition(1) \
+	# 	.where((result.prediction == 0.0) & (result.label == 1.0)) \
+	# 	.write.mode("overwrite").option("header", "true").csv("s3a://ph-max-auto/2020-08-11/BPBatchDAG/refactor/alfred/tmp/dt_predictions/false_negative")
+	# df_true_positive.orderBy("id").repartition(1) \
+	# 	.where((result.prediction == 1.0) & (result.label == 0.0)) \
+	# 	.write.mode("overwrite").option("header", "true").csv("s3a://ph-max-auto/2020-08-11/BPBatchDAG/refactor/alfred/tmp/dt_predictions/false_positive")
+	# for output
+	# df_true_positive.orderBy("id").repartition(1) \
+	# 	.where((result.prediction == 1.0) & (result.label == 0.0)) \
+	# 	.write.mode("overwrite").option("header", "true").csv("s3a://ph-max-auto/2020-08-11/BPBatchDAG/refactor/alfred/tmp/dt_predictions/prediction")
 
 	# 6. 在未匹配数据中，在剩下的数据中，机器给出建议
-	
+	df_true_positive = df_true_positive.select("id").distinct()
+	id_local = df_true_positive.toPandas()["id"].tolist()
+
+	df_candidate = result.where(~result.id.isin(id_local))
+	df_candidate = similarity(df_candidate)
+
+	df_prediction_se = df_candidate.where((df_candidate.SIMILARITY > 5.0) & (df_candidate.RANK == 1))
+	count_prediction_se = df_prediction_se.count()
+	print("第二轮总量= " + str(count_prediction_se))
+
+	df_prediction_se = hit_place_prediction(df_prediction_se, 1)
+	df_prediction_se_1_tp = df_prediction_se.where((df_prediction_se.prediction_1 == 1.0) & (df_prediction_se.prediction_1 == 1.0))
+	df_prediction_se_1_fp = df_prediction_se.where((df_prediction_se.prediction_1 == 1.0) & (df_prediction_se.label == 0.0))
+	positive_hit_1_tp = df_prediction_se_1_tp.count()
+	positive_hit_1_fp = df_prediction_se_1_fp.count()
+	print("第一正确匹配pack id 数量 = " + str(positive_hit_1_tp))
+	print("第一正确匹配pack id presicion = " + str(positive_hit_1_tp / (positive_hit_1_fp + positive_hit_1_tp)))
+
+	# 7. 两轮估算总量
+	ph_positive_prodict = ph_positive_prodict + positive_hit_1_tp + positive_hit_1_fp
+	print(ph_positive_prodict)
+	ph_positive_hit = ph_positive_hit + positive_hit_1_tp
+	print(ph_positive_hit)
+	# ph_negetive_hit = result.where(result.prediction != result.label).count()
+	print("第二轮 Pharbers Test set accuracy = " + str(ph_positive_hit / ph_total))
+	print("第二轮 Pharbers Test set precision = " + str(ph_positive_hit / ph_positive_prodict))
+
+
+	# 8. 第三轮，剩下的给出猜测
+	# 9. 最后一步，给出完全没匹配的结果
