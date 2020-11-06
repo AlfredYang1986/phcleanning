2fa7060ae3131c554e87e92e7cf1df80dc1805a4
zyyin <alfred>
2020-11-06 15:13:26 +0800

    修改冲突

--- tf_idf_mnf_with_cosine.py
+++ tf_idf_mnf_with_cosine.py
@@ -12,8 +12,10 @@ from interfere import *
 from pdu_feature import *
 from pyspark.sql.types import *
 from pyspark.sql.functions import pandas_udf
+from pyspark.sql.functions import udf
 from pyspark.sql.functions import when
 from pyspark.sql.functions import first
+from pyspark.sql.functions import array
 from pyspark.ml.linalg import Vectors, VectorUDT
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.feature import HashingTF, IDF, Tokenizer
@@ -23,9 +25,13 @@ from pyspark.ml.classification import DecisionTreeClassifier
 from pyspark.ml.feature import StringIndexer, VectorIndexer
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 import pandas as pd
+import numpy
+from math import sqrt
 import jieba
 import jieba.posseg as pseg
 import jieba.analyse as analyse
+# from nltk.cluster.util import cosine_distance
+# from pyspark.ml.functions import vector_to_array
 
 
 def prepare():
@@ -87,6 +93,13 @@ def manifacture_name_pseg_cut(mnf):
 	return df["MANUFACTURER_NAME_STANDARD_WORDS"]
 
 
+@udf
+def cosine_distance_between_mnf(array):
+	u = array[0].toArray()
+	v = array[1].toArray()
+	return float(numpy.dot(u, v) / (sqrt(numpy.dot(u, u)) * sqrt(numpy.dot(v, v))))
+
+
 if __name__ == '__main__':
 	spark = prepare()
 
@@ -121,60 +134,41 @@ if __name__ == '__main__':
 
 	# 5. 利用原有的crossjoin 数据构建，公司的label
 	df_result = load_training_data(spark)  # 待清洗数据
+	df_result = df_result.withColumn("MANUFACTURER_NAME_EN_STANDARD", manifacture_name_en_standify(df_result.MANUFACTURER_NAME_EN_STANDARD))
 
 	# 为生产厂商构建新label
-	df_mnf_label = df_result.where(df_result.label == 1.0).select("id", "MANUFACTURER_NAME", "MANUFACTURER_NAME_STANDARD","MANUFACTURER_NAME_EN_STANDARD")
-	df_mnf_label = df_mnf_label.groupBy("id").agg(first(df_mnf_label.MANUFACTURER_NAME).alias("MANUFACTURER_NAME_ANSWER"), \
-													first(df_mnf_label.MANUFACTURER_NAME_STANDARD).alias("MANUFACTURER_NAME_STANDARD_ANSWER"), \
-													first(df_mnf_label.MANUFACTURER_NAME_EN_STANDARD).alias("MANUFACTURER_NAME_EN_STANDARD_ANSWER"))
-	df_result = df_result.join(df_mnf_label, how="left", on="id")
-	df_result = df_result.withColumn("mnf_label", when(
-													(df_result.MANUFACTURER_NAME_STANDARD == df_result.MANUFACTURER_NAME_STANDARD_ANSWER) |
-													(df_result.MANUFACTURER_NAME_EN_STANDARD == df_result.MANUFACTURER_NAME_EN_STANDARD_ANSWER), 1.0).otherwise(0.0))
-	# df_result.select("id", "MANUFACTURER_NAME", "MANUFACTURER_NAME_STANDARD","MANUFACTURER_NAME_STANDARD_ANSWER", "mnf_label").show()
-	df_result = df_result.drop("MANUFACTURER_NAME_ANSWER", "MANUFACTURER_NAME_STANDARD_ANSWER", "MANUFACTURER_NAME_EN_STANDARD_ANSWER")
+	# df_mnf_label = df_result.where(df_result.label == 1.0).select("id", "MANUFACTURER_NAME", "MANUFACTURER_NAME_STANDARD","MANUFACTURER_NAME_EN_STANDARD")
+	# df_mnf_label = df_mnf_label.groupBy("id").agg(first(df_mnf_label.MANUFACTURER_NAME).alias("MANUFACTURER_NAME_ANSWER"), \
+	# 												first(df_mnf_label.MANUFACTURER_NAME_STANDARD).alias("MANUFACTURER_NAME_STANDARD_ANSWER"), \
+	# 												first(df_mnf_label.MANUFACTURER_NAME_EN_STANDARD).alias("MANUFACTURER_NAME_EN_STANDARD_ANSWER"))
+	# df_result = df_result.join(df_mnf_label, how="left", on="id")
+	# df_result = df_result.withColumn("mnf_label", when(
+	# 												(df_result.MANUFACTURER_NAME_STANDARD == df_result.MANUFACTURER_NAME_STANDARD_ANSWER) |
+	# 												(df_result.MANUFACTURER_NAME_EN_STANDARD == df_result.MANUFACTURER_NAME_EN_STANDARD_ANSWER), 1.0).otherwise(0.0))
+	# # df_result.select("id", "MANUFACTURER_NAME", "MANUFACTURER_NAME_STANDARD","MANUFACTURER_NAME_STANDARD_ANSWER", "mnf_label").show()
+	# df_result = df_result.drop("MANUFACTURER_NAME_ANSWER", "MANUFACTURER_NAME_STANDARD_ANSWER", "MANUFACTURER_NAME_EN_STANDARD_ANSWER")
 
 	# 5.1 split the words
-	print("split words")
 	# tokenizer = Tokenizer(inputCol="MANUFACTURER_NAME_EN_STANDARD", outputCol="MANUFACTURER_NAME_EN_WORDS")
 	df_result = tokenizer.transform(df_result)
 
-	# 3. 中文的分词，jieba
+	# 5.2. 中文的分词，jieba
 	df_result = df_result.withColumn("MANUFACTURER_NAME_WORDS", manifacture_name_pseg_cut(df_result.MANUFACTURER_NAME_STANDARD))
+	df_result = idf_model.transform(df_result)
+	df_result = df_result.withColumnRenamed("features_mnf_cn", "features_mnf_cn_standard")
 	# df_result.select("MANUFACTURER_NAME_STANDARD", "MANUFACTURER_NAME_WORDS", "MANUFACTURER_NAME_EN_STANDARD", "MANUFACTURER_NAME_EN_WORDS").show(truncate=False)
 
+	# 5.3. 带匹配的分词
+	df_result = df_result.withColumn("MANUFACTURER_NAME_WORDS", manifacture_name_pseg_cut(df_result.MANUFACTURER_NAME))
+	df_result = df_result.drop("raw_features_mnf_en", "raw_features_mnf_cn", "features_mnf_en", "features_mnf_cn")
 	df_result = idf_model.transform(df_result)
-	df_result.show()
+	df_result = df_result.drop("raw_features_mnf_en", "raw_features_mnf_cn", "features_mnf_en")
+	df_result.show(truncate=False)
+	# df_result = df_result.withColumn("features_mnf_cn_standard", df_result.features_mnf_cn_standard.toArray()) \
+							# .withColumn("features_mnf_cn", df_result.features_mnf_cn.toArray())
 
-	# 6. 开始利用idf来机器学习单一列
-	# 构建决策树model
-	df_cleanning = load_training_data(spark).select("id").distinct()
-	# Split the data into training and test sets (30% held out for testing)
-	(df_training, df_test) = df_cleanning.randomSplit([0.7, 0.3])
+	# 6. 直接是用cosine similarity 不要用机器学习 多此一举
+	df_result = df_result.limit(100).withColumn("CONSINE_SIMILARITY", cosine_distance_between_mnf(array(df_result.features_mnf_cn, df_result.features_mnf_cn_standard)))
+	df_result.select("id", "label", "MANUFACTURER_NAME", "MANUFACTURER_NAME_STANDARD", "CONSINE_SIMILARITY", "EFFTIVENESS_MANUFACTURER").show()
+	df_result.select("id", "label", "MANUFACTURER_NAME", "MANUFACTURER_NAME_STANDARD", "EFFTIVENESS_MANUFACTURER", "features_mnf_cn_standard", "features_mnf_cn").show(truncate=False)
 
-	# labelIndexer = StringIndexer(inputCol="mnf_label", outputCol="indexedLabel")
-	# featureIndexer = VectorIndexer(inputCol="features_mnf_cn", outputCol="indexedFeatures", maxCategories=6)
-
-	# 6.1 构建训练集合
-	df_training = df_training.join(df_result, how="left", on="id")
-	# 6.2 构建测试集合
-	df_test = df_test.join(df_result, how="left", on="id")
-
-	# Train a DecisionTree model.
-	dt = DecisionTreeClassifier(labelCol="mnf_label", featuresCol="features_mnf_cn")
-
-	# Chain indexers and tree in a Pipeline
-	pipeline_mnf_dt = Pipeline(stages=[dt])
-
-	# Train model.  This also runs the indexers.
-	mnf_model = pipeline_mnf_dt.fit(df_training)
-	mnf_model.write().overwrite().save("s3a://ph-max-auto/2020-08-11/BPBatchDAG/refactor/alfred/mnf_model")
-
-	# Make predictions.
-	df_predictions = mnf_model.transform(df_test)
-
-	evaluator = MulticlassClassificationEvaluator(
-		labelCol="mnf_label", predictionCol="prediction", metricName="accuracy")
-	accuracy = evaluator.evaluate(df_predictions)
-	print("Test Error = %g " % (1.0 - accuracy))
-	print("Test Accuracy = %g " % accuracy)
